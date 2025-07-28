423f50e323c5b3feabc40d6fa24e08c4dd1bfcad
Kaushik Roy <84892601+kaushikroy98@users.noreply.github.com>
2025-07-28 19:55:29 +0530

    Update Day_3.sql

--- Day_3.sql
+++ Day_3.sql
@@ -308,7 +308,15 @@ join actor_tv at on at.actor_id = am.actor_id;
 • The order of your results doesn't matter.
 • If there are ties, return just one of them.*/
 
-
+select c.name, sum(p.amount) from payment p
+join rental r on r.rental_id = p.rental_id
+join inventory i on i.inventory_id = r.inventory_id
+join film f on f.film_id = i.film_id
+join film_category fc on fc.film_id = f.film_id
+join category c on c.category_id = fc.category_id
+group by 1
+order by 2 desc
+limit 3;
 
 
 
