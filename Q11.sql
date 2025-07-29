3168b5db0bc99703d68befa173159fc429467a9d
Kaushik Roy <84892601+kaushikroy98@users.noreply.github.com>
2025-07-29 19:25:13 +0530

    Delete Q11.sql

--- Q11.sql
+++
@@ -1,9 +0,0 @@
--- Q11. Percentile, decile, quartile by MPAA rating
-
-select title, replacement_cost as cost,rating,
-
-ntile(100) over(partition by rating order by replacement_cost) as percentile,
-ntile(10) over(partition by rating order by replacement_cost) as decile,
-ntile(4) over(partition by rating order by replacement_cost) as quartile
-
-from film ;
